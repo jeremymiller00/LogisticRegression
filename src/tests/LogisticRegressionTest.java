94f52d4b9ea1eeef2624b081a596d9f122c37ace
Jeremy Miller <jeremymiller00@gmail.com>
2019-05-02 14:30:23 -0700

    add basic test file

---
+++ src/tests/LogisticRegressionTest.java
@@ -0,0 +1,50 @@
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class LogisticRegressionTest extends LogisticRegression {
+
+	@Test
+	public void testSetCoefficients() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCoefficients() {
+		fail("Not yet implemented");
+	}
+
+    public void testCalculateLRScore() {
+        final ArrayList<Double> sentenceVector = new ArrayList<Double>();
+        sentenceVector.add(1.0);
+        sentenceVector.add(2.0);
+        sentenceVector.add(3.0);
+        
+        final ArrayList<Double> coefficients = new ArrayList<Double>();
+        coefficients.add(4.0);
+        coefficients.add(5.0);
+        coefficients.add(6.0);
+        
+        final Double intercept = 1.0;
+
+        final Double expected = 17.0;
+        final Double actual = LogisticRegression.calculateLRScore(sentenceVector, coefficients, intercept);
+
+        Assert.assertEquals(actual, expected);        
+    }
+
+    public void testLogit() {
+        final Double exponent1 = 0.0;
+        final double expected1 = 0.5;
+        final Double actual1 = LogisticRegression.logit(exponent1);
+        Assert.assertEquals(expected1, actual1);
+
+        final Double exponent2 = 1.0;
+        final Double expected2 = Math.exp(1) / (1 + Math.exp(1));
+        final Double actual2 = LogisticRegression.logit(exponent2);
+        Assert.assertEquals(expected2, actual2);
+
+    }
+}
